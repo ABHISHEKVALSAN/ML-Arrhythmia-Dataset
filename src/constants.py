f48d2c06ef178f5a19cd6493ca87ce0ad9edc791
Abhishek Valsan <abhishek.valsan@myntra.com>
2021-06-05 18:11:13 +0530

    constants file

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,78 @@
-DI_waves = []
-DII_waves = []
-DII_waves = []
-AVF = []
-AVL = []
-AVR = []
-V1 = []
-V2 = []
-V3 = []
-V4 = []
-V5 = []
-V6 = []
+GIVEN=['age','sex','height','weight','qrs_duration','pr_interval',
+		'qt_interval','t_interval','p_interval','qrs','T','P',
+		'QRST','J','heart_rate']
+DI = [	'DI_q_wave','DI_r_wave','DI_s_wave','DI_R_wave','DI_S_wave',
+		'DI_no_intr_defl','DI_exis_ragg_r_wave','DI_exis_diph_der_r_wave',
+		'DI_exis_ragg_p_wave','DI_exis_diph_der_p_wave','DI_exis_ragg_t_wave',
+		'DI_exis_diph_der_t_wave','DI_amp_JJ_wave','DI_amp_q_wave',
+		'DI_amp_r_wave','DI_amp_s_wave','DI_amp_R_wave','DI_amp_S_wave',
+		'DI_amp_p_wave','DI_amp_t_wave','DI_qrsa','DI_qrsta']
+DII = [	'DII_q_wave','DII_r_wave','DII_s_wave','DII_R_wave','DII_S_wave',
+		'DII_no_intr_defl','DII_exis_ragg_r_wave','DII_exis_diph_der_r_wave',
+		'DII_exis_ragg_p_wave','DII_exis_diph_der_p_wave','DII_exis_ragg_t_wave',
+		'DII_exis_diph_der_t_wave','DII_amp_JJ_wave','DII_amp_q_wave',
+		'DII_amp_r_wave','DII_amp_s_wave','DII_amp_R_wave','DII_amp_S_wave',
+		'DII_amp_p_wave','DII_amp_t_wave','DII_qrsa','DII_qrsta']
+DIII = [	'DIII_q_wave','DIII_r_wave','DIII_s_wave','DIII_R_wave','DIII_S_wave',
+		'DIII_no_intr_defl','DIII_exis_ragg_r_wave','DIII_exis_diph_der_r_wave',
+		'DIII_exis_ragg_p_wave','DIII_exis_diph_der_p_wave','DIII_exis_ragg_t_wave',
+		'DIII_exis_diph_der_t_wave','DIII_amp_JJ_wave','DIII_amp_q_wave',
+		'DIII_amp_r_wave','DIII_amp_s_wave','DIII_amp_R_wave','DIII_amp_S_wave',
+		'DIII_amp_p_wave','DIII_amp_t_wave','DIII_qrsa','DIII_qrsta']
+AVR = [	'AVR_q_wave','AVR_r_wave','AVR_s_wave','AVR_R_wave','AVR_S_wave',
+		'AVR_no_intr_defl','AVR_exis_ragg_r_wave','AVR_exis_diph_der_r_wave',
+		'AVR_exis_ragg_p_wave','AVR_exis_diph_der_p_wave','AVR_exis_ragg_t_wave',
+		'AVR_exis_diph_der_t_wave','AVR_amp_JJ_wave','AVR_amp_q_wave',
+		'AVR_amp_r_wave','AVR_amp_s_wave','AVR_amp_R_wave','AVR_amp_S_wave',
+		'AVR_amp_p_wave','AVR_amp_t_wave','AVR_qrsa','AVR_qrsta']
+AVF = [	'AVF_q_wave','AVF_r_wave','AVF_s_wave','AVF_R_wave','AVF_S_wave',
+		'AVF_no_intr_defl','AVF_exis_ragg_r_wave','AVF_exis_diph_der_r_wave',
+		'AVF_exis_ragg_p_wave','AVF_exis_diph_der_p_wave','AVF_exis_ragg_t_wave',
+		'AVF_exis_diph_der_t_wave','AVF_amp_JJ_wave','AVF_amp_q_wave',
+		'AVF_amp_r_wave','AVF_amp_s_wave','AVF_amp_R_wave','AVF_amp_S_wave',
+		'AVF_amp_p_wave','AVF_amp_t_wave','AVF_qrsa','AVF_qrsta']
+AVL = [	'AVL_q_wave','AVL_r_wave','AVL_s_wave','AVL_R_wave','AVL_S_wave',
+		'AVL_no_intr_defl','AVL_exis_ragg_r_wave','AVL_exis_diph_der_r_wave',
+		'AVL_exis_ragg_p_wave','AVL_exis_diph_der_p_wave','AVL_exis_ragg_t_wave',
+		'AVL_exis_diph_der_t_wave','AVL_amp_JJ_wave','AVL_amp_q_wave',
+		'AVL_amp_r_wave','AVL_amp_s_wave','AVL_amp_R_wave','AVL_amp_S_wave',
+		'AVL_amp_p_wave','AVL_amp_t_wave','AVL_qrsa','AVL_qrsta']
+V1 = [	'V1_q_wave','V1_r_wave','V1_s_wave','V1_R_wave','V1_S_wave',
+		'V1_no_intr_defl','V1_exis_ragg_r_wave','V1_exis_diph_der_r_wave',
+		'V1_exis_ragg_p_wave','V1_exis_diph_der_p_wave','V1_exis_ragg_t_wave',
+		'V1_exis_diph_der_t_wave','V1_amp_JJ_wave','V1_amp_q_wave',
+		'V1_amp_r_wave','V1_amp_s_wave','V1_amp_R_wave','V1_amp_S_wave',
+		'V1_amp_p_wave','V1_amp_t_wave','V1_qrsa','V1_qrsta']
+V2 = [	'V2_q_wave','V2_r_wave','V2_s_wave','V2_R_wave','V2_S_wave',
+		'V2_no_intr_defl','V2_exis_ragg_r_wave','V2_exis_diph_der_r_wave',
+		'V2_exis_ragg_p_wave','V2_exis_diph_der_p_wave','V2_exis_ragg_t_wave',
+		'V2_exis_diph_der_t_wave','V2_amp_JJ_wave','V2_amp_q_wave',
+		'V2_amp_r_wave','V2_amp_s_wave','V2_amp_R_wave','V2_amp_S_wave',
+		'V2_amp_p_wave','V2_amp_t_wave','V2_qrsa','V2_qrsta']
+V3 = [	'V3_q_wave','V3_r_wave','V3_s_wave','V3_R_wave','V3_S_wave',
+		'V3_no_intr_defl','V3_exis_ragg_r_wave','V3_exis_diph_der_r_wave',
+		'V3_exis_ragg_p_wave','V3_exis_diph_der_p_wave','V3_exis_ragg_t_wave',
+		'V3_exis_diph_der_t_wave','V3_amp_JJ_wave','V3_amp_q_wave',
+		'V3_amp_r_wave','V3_amp_s_wave','V3_amp_R_wave','V3_amp_S_wave',
+		'V3_amp_p_wave','V3_amp_t_wave','V3_qrsa','V3_qrsta']
+V4 = [	'V4_q_wave','V4_r_wave','V4_s_wave','V4_R_wave','V4_S_wave',
+		'V4_no_intr_defl','V4_exis_ragg_r_wave','V4_exis_diph_der_r_wave',
+		'V4_exis_ragg_p_wave','V4_exis_diph_der_p_wave','V4_exis_ragg_t_wave',
+		'V4_exis_diph_der_t_wave','V4_amp_JJ_wave','V4_amp_q_wave',
+		'V4_amp_r_wave','V4_amp_s_wave','V4_amp_R_wave','V4_amp_S_wave',
+		'V4_amp_p_wave','V4_amp_t_wave','V4_qrsa','V4_qrsta']
+V5 = [	'V5_q_wave','V5_r_wave','V5_s_wave','V5_R_wave','V5_S_wave',
+		'V5_no_intr_defl','V5_exis_ragg_r_wave','V5_exis_diph_der_r_wave',
+		'V5_exis_ragg_p_wave','V5_exis_diph_der_p_wave','V5_exis_ragg_t_wave',
+		'V5_exis_diph_der_t_wave','V5_amp_JJ_wave','V5_amp_q_wave',
+		'V5_amp_r_wave','V5_amp_s_wave','V5_amp_R_wave','V5_amp_S_wave',
+		'V5_amp_p_wave','V5_amp_t_wave','V5_qrsa','V5_qrsta']
+V6 = [	'V6_q_wave','V6_r_wave','V6_s_wave','V6_R_wave','V6_S_wave',
+		'V6_no_intr_defl','V6_exis_ragg_r_wave','V6_exis_diph_der_r_wave',
+		'V6_exis_ragg_p_wave','V6_exis_diph_der_p_wave','V6_exis_ragg_t_wave',
+		'V6_exis_diph_der_t_wave','V6_amp_JJ_wave','V6_amp_q_wave',
+		'V6_amp_r_wave','V6_amp_s_wave','V6_amp_R_wave','V6_amp_S_wave',
+		'V6_amp_p_wave','V6_amp_t_wave','V6_qrsa','V6_qrsta']
+FEATURES = GIVEN+DI+DII+DIII+AVR+AVL+AVF+V1+V2+V3+V4+V5+V6
+LABEL = ['diagnosis']
+HEADERS = FEATURES + LABEL
